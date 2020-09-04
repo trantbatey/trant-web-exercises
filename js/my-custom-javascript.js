5b42d44949ad5e4ec202e39ec23d7aaf38f6efce
trantbatey <trantbatey@gmail.com>
2020-09-04 11:47:23 -0500

    Added IFEE and Ready Function to JQuery Mouse Events.

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -8,32 +8,40 @@
 //
 //     4. Set all li text color to red when the mouse is hovering; reset to black when it is not.
 
+(function () { // IFEE
+    "use strict"
+    $().ready(function () {
 
-"use strict";
+        "use strict";
 
 //     2. Add jQuery code that will change the background color of an h1 element when clicked.
-$('h1').click(function () {
-    $(this).css('background-color', '#ff0')
-})
+        $('h1').click(function () {
+            $(this).css('background-color', '#ff0')
+        })
 
 //     3. Make all paragraphs have a font size of 18px when they are double clicked.
-function p_dblHandler() {
-    $(this).css('font-size', '18px')
-}
-$('p').dblclick(p_dblHandler)
+        function p_dblHandler() {
+            $(this).css('font-size', '18px')
+        }
+
+        $('p').dblclick(p_dblHandler)
 
 //     4. Set all li text color to red when the mouse is hovering; reset to black when it is not.
-let oldTextColor
-$('li').hover(
-    function() {
-        oldTextColor = $(this).css('color')
-        console.log(oldTextColor)
-        $(this).css('color', 'red');
-    },
-    function() {
-        console.log(oldTextColor)
-        $(this).css('color', oldTextColor);
-    }
-);
+        let oldTextColor
+        $('li').hover(
+            function () {
+                oldTextColor = $(this).css('color')
+                console.log(oldTextColor)
+                $(this).css('color', 'red');
+            },
+            function () {
+                console.log(oldTextColor)
+                $(this).css('color', oldTextColor);
+            }
+        );
 
-$('li').click(function () { oldTextColor = 'green'})
+        $('li').click(function () {
+            oldTextColor = 'green'
+        })
+    });
+})();
